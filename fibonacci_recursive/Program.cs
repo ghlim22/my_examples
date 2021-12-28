636751c7767811192c1b89962f4ccea1c09ae3ed
ghlim22 <hyun22@mail.hongik.ac.kr>
2021-12-28 17:11:23 +0900

    add fibonacci_recursive

---
+++ fibonacci_recursive/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace fibonacci_recursive
+{
+    class Program
+    {
+        public static int[] GetFibonacci(int n)
+        {
+            int[] sequence = new int[n];
+            sequence[0] = 1;
+            sequence[1] = 1; 
+            for (int i = 2; i < n; ++i)
+            {
+                sequence[i] = sequence[i - 1] + sequence[i - 2];
+            }
+            return sequence;
+        }
+
+        public static int GetFibonacciRecursive(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            return GetFibonacciRecursive(n-1) + GetFibonacciRecursive(n-2);
+        }
+        public static void Main(string[] args)
+        {
+            int n = 10;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int[] sequence = GetFibonacci(n);
+            for (int i = 0; i < sequence.Length; ++i)
+            {
+                Console.Write("{0, 7}{1}", sequence[i], (i % 10 == 9) ? "\n" : "");
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.ElapsedMilliseconds}ms");
+            watch.Reset();
+
+            watch.Start();
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write("{0, 7}{1}", GetFibonacciRecursive(n), (i % 10 == 9) ? "\n" : "");
+            }
+            watch.Stop();
+            Console.WriteLine($"Time: {watch.ElapsedMilliseconds}ms");
+            watch.Reset();
+
+
+        }
+    }
+}
